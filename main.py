7a0431ae9bc49778b8ec6340e7e2fc7d91b1916c
Konstanty <61802175+KonstantyOdwazny@users.noreply.github.com>
2022-10-04 16:48:15 +0000

    Next lessons

--- main.py
+++ main.py
@@ -12,9 +12,16 @@
 # print( 6 % 4) # operacja modulo (reszta z dzielenia)
 
 # Math functions:
-print(round(3.5)) # zaokraglanie 
-print(abs(-20))
+# print(round(3.5)) # zaokraglanie 
+# print(abs(-20))
 
 # operator precedence - pierwszenstwo operatora
-print(20 - 3*4)
+#1. ()
+#2. **
+#3. * and /
+#4. + and -
+# print((20-3)+5 - 3*4)
 
+# Optional bin() and complex
+print(bin(5)) # zamiana liczb na liczby binarne
+print(int('0b101',2)) # zamiana liczb binarnych na dziesietne
