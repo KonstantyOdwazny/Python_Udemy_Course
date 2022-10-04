0958ae67833fc38149eafd7252e3e46178f75172
Konstanty <61802175+KonstantyOdwazny@users.noreply.github.com>
2022-10-04 16:35:11 +0000

    Numbers

---
+++ basic.py
@@ -0,0 +1,20 @@
+# name = input('What is your name?\n')
+# print(f'Hello {name}!')
+# Fundamental Data Types:
+# a) Numbers:
+# int, float
+# print(2+4," Type:", type(2+4)) #int
+# print(2-4," Type:", type(2-4)) # int
+# print(2*4," Type:", type(2*4)) # int
+# print(2/4, " Type:", type(2/4)) # float
+# print( 2**2) # ** potega
+# print( 6 // 4) # zwraca integer z dzielenia
+# print( 6 % 4) # operacja modulo (reszta z dzielenia)
+
+# Math functions:
+print(round(3.5)) # zaokraglanie 
+print(abs(-20))
+
+# operator precedence - pierwszenstwo operatora
+print(20 - 3*4)
+
