ce4e30f42e408191f8253b65a44bb60be385861d
KonstantyOdwazny <61802175+KonstantyOdwazny@users.noreply.github.com>
2022-10-14 15:24:19 +0200

    Rysowanie linii na video

--- OpenCV_tutorial/RoadLineDetection.py
+++ OpenCV_tutorial/RoadLineDetection.py
@@ -45,6 +45,14 @@ def tracbar_canny_threshold_checking(img_):
             break
     cv2.destroyAllWindows()
 def draw_lines(img1, lines):
+    """Funkcja, która rysuje linie znalezione na obrazie
+    Args:
+        img1 (array): Obraz, na którym rysujemy linie
+        lines (array): vector lini do narysowania
+
+    Returns:
+        array: Obraz z naniesionymi liniami
+    """    
     img1 = np.copy(img1)
     blank_image = np.zeros((img1.shape[0], img1.shape[1], 3), np.uint8)
     
@@ -53,38 +61,66 @@ def draw_lines(img1, lines):
             cv2.line(blank_image, (x1,y1), (x2,y2), (0,255,0), 3)
     img1 = cv2.addWeighted(img1, 0.8, blank_image, 1, 0.0)
     return img1
+def image_detection(image):
+    """Wykrywanie linii na obrazie
+    Returns:
+        array: Obraz na którym narysowalismy linie
+    Args:
+        image (array): Obraz na którym wykrywamy linie
+    """    
+    #1.  Define ROI - region zainteresowania 
+    print(image.shape)
+    height = image.shape[0]
+    width = image.shape[1]
+    roi_verticies = [
+        (0,height),
+        (width/2, height/4),
+        (width, height)
+    ]
+    # 2. Canny edge detection
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    # tracbar_canny_threshold_checking(gray) #sprawdzenie ustawien Thresoldingu
+    canny_image = cv2.Canny(gray, 50, 40) # th1 : 50, th2 : 40 - najlepsze ustawienia dla tego obrazu
+    cropp_image = region_of_intrest(canny_image, np.array([roi_verticies], np.int32)) # Obraz z ROI (reszta czarna)
+
+    # 3. Color lines
+    lines = cv2.HoughLinesP(cropp_image, 3, np.pi/90, 30, lines=np.array([]), minLineLength=20,maxLineGap=25)
+    image_with_lines = draw_lines(image, lines)
+    plt.subplot(1,3,1) 
+    plt.imshow(canny_image)
+    plt.title('Canny Image')
+    plt.subplot(1,3,2)
+    plt.imshow(cropp_image)
+    plt.title('Cropp Image')
+    plt.subplot(1,3,3)
+    plt.imshow(image_with_lines)
+    plt.title('With Line Image')
+    plt.show()
+    return image_with_lines
+    
+def video_detection(cap):
+    """Funkcja, która rysuje linie na video
+    Args:
+        cap (array): Video
+    """    
+    while cap.isOpened():
+        ret, frame = cap.read()
+        frame = image_detection(frame)
+        cv2.imshow("Viedo frame",frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
     
     
-image = cv2.imread('images/road1.jpg')
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # zeby uzyc matplotlib
+    
+if __name__ == "__main__":
+    image = cv2.imread('images/road1.jpg')
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # zeby uzyc matplotlib
+    image = image_detection(image)
+    
+    # cap = cv2.VideoCapture('videos/road_movie.mp4')
+    # video_detection(cap)
+    
 
 
-#1.  Define ROI - region zainteresowania 
-print(image.shape)
-height = image.shape[0]
-width = image.shape[1]
-roi_verticies = [
-    (0,height),
-    (width/2, height/4),
-    (width, height)
-]
-# 2. Canny edge detection
-gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-# tracbar_canny_threshold_checking(gray) #sprawdzenie ustawien Thresoldingu
-canny_image = cv2.Canny(gray, 50, 40) # th1 : 50, th2 : 40 - najlepsze ustawienia dla tego obrazu
-cropp_image = region_of_intrest(canny_image, np.array([roi_verticies], np.int32)) # Obraz z ROI (reszta czarna)
-
-# 3. Color lines
-lines = cv2.HoughLinesP(cropp_image, 3, np.pi/90, 30, lines=np.array([]), minLineLength=20,maxLineGap=25)
-image_with_lines = draw_lines(image, lines)
-
-plt.subplot(1,3,1) 
-plt.imshow(canny_image)
-plt.title('Canny Image')
-plt.subplot(1,3,2)
-plt.imshow(cropp_image)
-plt.title('Cropp Image')
-plt.subplot(1,3,3)
-plt.imshow(image_with_lines)
-plt.title('With Line Image')
-plt.show()
